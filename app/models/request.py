b00d6d0125a2e2a2bb4cb2d6acee34b0a0f3a907
bloodeRok <spoluyakhtov99@gmail.com>
2024-09-26 15:06:45 +0300

    feat(bad_guys): Added Request model.

---
+++ app/models/request.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+
+from . import Base
+
+
+class Request(Base):
+    __tablename__ = "request"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(TIMESTAMP, default=datetime.utcnow)
+    inn = Column(String, nullable=False)
+    nickname = Column(String, nullable=False)
+    fio = Column(String, nullable=False)
+    office = Column(String, nullable=False)
+    module = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<User(" \
+               f"id={self.id}, " \
+               f"date={self.date}, " \
+               f"inn={self.inn}," \
+               f"nickname={self.nickname}, " \
+               f"fio={self.fio}," \
+               f"office={self.office}, " \
+               f"module={self.module}" \
+               f")>"
+
+    def __str__(self):
+        return self.__repr__()
